51edcc742dbe0f7406a903565a39348dd7bf2f17
Alberto Fabbri <alberto_fabbri.git@yahoo.com>
2024-12-06 10:27:36 +0100

    Improve color palette

--- Analysis/Color_palettes.R
+++ Analysis/Color_palettes.R
@@ -11,48 +11,54 @@ patient_colors <- c(
 
 # Color vector for cell types, similar cell types have similar colors
 cell_type_colors <- c(
-  "Tumor" = "#C0C0C0",
-  # Lymphocites
+  
   # A type of immune cells that are made in the bone marrow and are found in the blood and in lymph tissue
-  "B cell" = "#D4819C",
-  "NK cell" = "#7790BD",
-  "Plasma" = "#955C97",
-  "Plasmablast" = "#B3709B",
-  "T cell CD4" = "#315A2F",
-  "T cell CD8" = "#64AB73",
-  "T cell regulatory" = "#526F55",
-  # Myeloid cells
+  "Lymphocites" = "#30D5C8",
+    "B cell" = "#D4819C",
+    "NK cell" = "#7790BD",
+    "Plasma" = "#955C97",
+    "Plasmablast" = "#B3709B",
+    "T cell CD4" = "#315A2F",
+    "T cell CD8" = "#64AB73",
+    "T cell regulatory" = "#526F55",
+  
   # Family of immune cells comprising monocytes, macrophages, myeloid dendritic cells (mDCs),
   # granulocytes, and mast cells that originate from a common myeloid progenitor in the bone marrow
-  "Dendritic cell" = "#5671B3",
-  "Macrophage" = "#55274E",
-  "Mast cell" = "#C8689F",
-  "Monocyte" = "#6E3987",
-  "Neutrophil" = "#9C6DA8",
-  "Plasmacytoid dendritic cell" = "#212222",
-  # Stromal cells
+  "Myeloids" = "#BF489F",
+    "Dendritic cell" = "#5671B3",
+    "Macrophage" = "#55274E",
+    "Mast cell" = "#C8689F",
+    "Monocyte" = "#6E3987",
+    "Neutrophil" = "#9C6DA8",
+    "Plasmacytoid dendritic cell" = "#212222",
+  
   # A type of cell that makes up certain types of connective tissue (supporting tissue that surrounds other tissues and organs)
-  "Endothelial" = "#EDBD39",
-  "Fibroblast" = "#C43522",
-  # Inhibitory neurons
-  "Inhibitory neuron A" = "#E3D26F",
-  "Inhibitory neuron B" = "#E3D26F",
-  "Inhibitory neuron C" = "#E3D26F",
-  # Excitatory neurons
-  "L2/3 neuron" = "#89808C",
-  "L4 neuron" = "#89808C",
-  "L6 neuron" = "#89808C",
-  # Glial cells
-  "Oligodendrocyte-like" = "#CEBACF",
-  "Oligodendrocyte precursor cell" = "#CEBACF",
-  "Oligodendrocyte" = "#CEBACF",
-  # Microglia cells
-  "Microglia A" = "#C08552",
-  "Microglia B" = "#C08552",
-  # Astrocyte cells
-  "Astrocyte A" = "#F9E0D9",
-  "Astrocyte B" = "#F9E0D9",
-  # Others
-  "Unknown" = "#f5ee59",
-  "Other" = "#B0B0B0"
+  "Stromal cells" = "#D48542",
+    "Endothelial" = "#EDBD39",
+    "Fibroblast" = "#C43522",
+    
+  "Brain cells" = "#111111",
+    # Inhibitory neurons
+    "Inhibitory neuron A" = "#E3D26F",
+    "Inhibitory neuron B" = "#E3D26F",
+    "Inhibitory neuron C" = "#E3D26F",
+    # Excitatory neurons
+    "L2/3 neuron" = "#89808C",
+    "L4 neuron" = "#89808C",
+    "L6 neuron" = "#89808C",
+    # Glial cells
+    "Oligodendrocyte-like" = "#CEBACF",
+    "Oligodendrocyte precursor cell" = "#CEBACF",
+    "Oligodendrocyte" = "#CEBACF",
+    # Microglia cells
+    "Microglia A" = "#C08552",
+    "Microglia B" = "#C08552",
+    # Astrocyte cells
+    "Astrocyte A" = "#F9E0D9",
+    "Astrocyte B" = "#F9E0D9",
+  
+  # Other cells
+    "Tumor" = "#C0C0C0",
+    "Unknown" = "#f5ee59",
+    "Other" = "#B0B0B0"
 )
